9b04f79245e82f105f9290bdd3d929798d806704
CP0070 <tripathi.cp07@gmail.com>
2025-11-11 18:35:01 +0530

    Remove old backend folder

--- backend/portfolio_backend/portfolio/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProjectListView, SkillListView
-
-urlpatterns = [
-    path('projects/', ProjectListView.as_view(), name='projects'),
-    path('skills/', SkillListView.as_view(), name='skills'),
-]
